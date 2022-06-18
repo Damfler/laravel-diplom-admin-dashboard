cfc180bdf3f2691fd85e24e433ac0bf24336b174
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2022-06-18 14:27:13 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Models\TradeOffer;
+use App\Models\TypeOffer;
+use App\Models\Stock;
+use App\Models\Brand;
+use App\Models\Slider;
+
 use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +25,13 @@ Route::redirect('/', '/dashboard');
 //dashboard routes
 Route::group(['middleware' => ['auth'], 'prefix' => 'dashboard', 'as' => 'admin.'], function () {
     //single action controllers
-    Route::get('/', HomeController::class)->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/tyre', [HomeController::class, 'index_tyre'])->name('home.tyre');
+    Route::get('/moto', [HomeController::class, 'index_moto'])->name('home.moto');
+    Route::get('/disk', [HomeController::class, 'index_disk'])->name('home.disk');
+    Route::get('/akb', [HomeController::class, 'index_akb'])->name('home.akb');
+
+    Route::post('/search', [HomeController::class, 'search'])->name('search');
 
     //link that return view, to get compoment from there
     Route::view('/buttons', 'admin.buttons')->name('buttons');
@@ -36,7 +48,38 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'dashboard', 'as' => 'admin.
         Route::view('/forgot-password-page', 'admin.pages.forgot-password')->name('forgot-password');
         Route::view('/login-page', 'admin.pages.login')->name('login');
     });
+
+    Route::get('/item/add', [HomeController::class, 'items'])->name('item.add');
+    Route::post('/item/add', [HomeController::class, 'items_add'])->name('item.add.post');
+    Route::get('/item/edit/{id}', [HomeController::class, 'items_edit'])->name('item.edit');
+    Route::get('/item/delete/{id}', function ($id) {
+        TradeOffer::where('id', $id)->delete();
+        return redirect(route('admin.item.add'))->withErrors('Товар успешно удален!', 'message');
+    })->name('item.delete');
+    Route::post('/item/edit/complete', [HomeController::class, 'items_edit_complete'])->name('item.edit.complete');
+
+    Route::get('/type', [HomeController::class, 'type'])->name('type.add');
+    Route::post('/type/add', [HomeController::class, 'type_add'])->name('type.add.post');
+    Route::get('/type/delete/{id}', function ($id) {
+        TypeOffer::where('id', $id)->delete();
+        return redirect(route('admin.type.add'))->withErrors('Тип успешно удален!', 'message');
+    })->name('type.delete');
+
+    Route::get('/brands', [HomeController::class, 'brands'])->name('brands.add');
+    Route::post('/brands/add', [HomeController::class, 'brand_add'])->name('brands.add.post');
+    Route::get('/brands/delete/{id}', function ($id) {
+        Brand::where('id', $id)->delete();
+        return redirect(route('admin.brands.add'))->withErrors('Бренд успешно удален!', 'message');
+    })->name('brands.delete');
+
+    Route::get('/slider', [HomeController::class, 'slider'])->name('slider.add');
+    Route::post('/slider/add', [HomeController::class, 'slider_add'])->name('slider.add.post');
+    Route::get('/slider/delete/{id}', function ($id) {
+        Slider::where('id', $id)->delete();
+        return redirect(route('admin.slider.add'))->withErrors('Слайд успешно удален!', 'message');
+    })->name('slider.delete');
 });
 
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/debug.php';
